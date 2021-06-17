243c2690876d2b0b79c64046a538fb7af2637084
BeerCSIM_AIT <st121032@ait.ac.th>
2021-06-17 17:42:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/duriantest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.duriantest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:duriantest/blocs/auth_bloc.dart';
+import 'package:duriantest/screens/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late StreamSubscription<User?> loginStateSubscription;
+  @override
+  void initState() {
+    var authBloc = Provider.of<AuthBloc>(context, listen: false);
+    loginStateSubscription = authBloc.currentUser.listen((fbUser) {
+      if (fbUser != null) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+        );
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    loginStateSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final authBloc = Provider.of<AuthBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SignInButton(
+              Buttons.Google,
+              onPressed: () => authBloc.loginGooble(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
